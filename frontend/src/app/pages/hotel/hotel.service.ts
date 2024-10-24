2c585ac7cbe96ffa3d93cdaa19a4b7eafbeb109d
Marlene <31359415+MNB-dev@users.noreply.github.com>
2024-10-24 01:38:52 -0300

    cambios

--- frontend/src/app/pages/hotel/hotel.service.ts
+++ frontend/src/app/pages/hotel/hotel.service.ts
@@ -2,18 +2,39 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Hotel } from '../../models/Hotel';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HotelService {
+  private apiUrl = `${environment.api}hoteles/`; 
 
   constructor(private readonly httpClient: HttpClient) { }
 
-  getAll() {
-    const url = `${environment.api}hoteles/`;
-    console.log(url);
-    return this.httpClient.get<Hotel[]>(url);
+  // Obtener todos los hoteles
+  getAll(): Observable<Hotel[]> {
+    return this.httpClient.get<Hotel[]>(this.apiUrl);
+  }
+
+  // Obtener un hotel por ID
+  getById(id: string): Observable<Hotel> {
+    return this.httpClient.get<Hotel>(`${this.apiUrl}${id}`);
+  }
+
+  // Crear un nuevo hotel
+  create(hotel: Hotel): Observable<Hotel> {
+    return this.httpClient.post<Hotel>(this.apiUrl, hotel);
+  }
+
+  // Actualizar un hotel
+  update(id: string, hotel: Hotel): Observable<Hotel> {
+    return this.httpClient.put<Hotel>(`${this.apiUrl}${id}`, hotel);
+  }
+
+  // Eliminar un hotel
+  delete(id: string): Observable<void> {
+    return this.httpClient.delete<void>(`${this.apiUrl}${id}`);
   }
 
 }
